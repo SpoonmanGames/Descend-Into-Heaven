770d036ba86ea22021cf328cc62893172df29c1f
Esteban Gaete <zoraskiller@gmail.com>
2015-08-16 11:13:24 -0400

    ADD: animaciones para los disparos y previa del script de disparo

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+    public bool Horizontal = true;
+    public bool IdaYVuelta = true;
+    [Space(10)]
+    public float LimiteInferior = 0.0f;
+    public float LimiteSuperior = 0.0f;
+    [Space(10)]
+    public GameObject Bullet;
+
+    void LateUpdate(){
+        float actualPosition;
+
+        if (Horizontal){
+            actualPosition = Bullet.transform.position.x;
+        } else {
+            actualPosition = Bullet.transform.position.y;
+        }
+
+        
+    }
+    
+}
