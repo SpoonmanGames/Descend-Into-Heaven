fd984c927d932f092b717e70f034abd4d80f56b4
HenBanKeN <nikolai.m.henriquez@gmail.com>
2015-08-17 16:36:56 -0300

    ADD: Musica y sonidos al Menu

--- Assets/Scripts/Controllers/MenuSelectorController.cs
+++ Assets/Scripts/Controllers/MenuSelectorController.cs
@@ -7,7 +7,19 @@ public class MenuSelectorController : MonoBehaviour {
     public List<Vector2> OptionPositions;
     public string SceneToLoad;
 
+    [Header("Audio Setup")]
+    public AudioClip moveSelectorSound;
+    public AudioClip pressSelectorSound;
+    public AudioSource selectorSource;
+
     private int _actualPosition = 0;
+
+    void Start() {
+
+        selectorSource = GetComponent<AudioSource>();
+
+    }
+
 
     void FixedUpdate() {
         if (_actualPosition >= OptionPositions.Count) {
@@ -22,14 +34,19 @@ public class MenuSelectorController : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
             _actualPosition--;
+            selectorSource.PlayOneShot(moveSelectorSound,1F);
         } else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
             _actualPosition++;
+            selectorSource.PlayOneShot(moveSelectorSound,1F);
         } else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl)) {
             ActionInMenu();
         }
 	}
 
     private void ActionInMenu() {
+
+        selectorSource.PlayOneShot(pressSelectorSound,1F);
+
         if (_actualPosition == 0) {
             Application.LoadLevel(SceneToLoad);
         } else if (_actualPosition == 1) {
