1d9a27e59703f1e515e59d8d0a8dfe9d35eeba9d
HenBanKeN <nikolai.m.henriquez@gmail.com>
2015-08-31 04:58:52 -0300

    agregando archivos que borre sin querer

---
+++ Assets/Editor/Player/CrossEnemyCustomEditor.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Player.CrossEnemy))]
+public class CrossEnemyCustomEditor : PlayerCustomEditor {
+
+    private const string _playerControllerError = "The Script won't work correctly if no Target Player is set.";
+    private const string _bulletToSpawnError = "The Script won't spawn anything if the Bullet to Spawn is not set.";
+    private const string _rangeDetectionSizeError = "The width and height MUST be bigger than 0. Otherwise it won't detect anything.";
+    private const string _choseShootDirectionsInfo = "The three normal direction of spawning the bullet are: left, up and right.";
+    private const string _shootAtTargetInfo = "The gameObject will shoot directly to the last detected position of the Target Player when in range.";
+    private const string _attackDelayError = "The Attack Delay must be bigger than 0.";
+    private const string _shootAlwaysInfo = "The Cross will shoot always with the attack delay given at the directions said.";
+
+    public override void ExtraGUI() {
+
+        SerializedObject myScript = new SerializedObject(target);
+        SerializedProperty PlayerController = myScript.FindProperty("PlayerController");
+        SerializedProperty BulletToSpawn = myScript.FindProperty("BulletToSpawn");
+        SerializedProperty AttackDelay = myScript.FindProperty("AttackDelay");
+        SerializedProperty ShootAlways = myScript.FindProperty("ShootAlways");
+        SerializedProperty DetectionBlock = myScript.FindProperty("DetectionBlock");
+        SerializedProperty ShootRangeBlock = myScript.FindProperty("ShootRangeBlock");
+        SerializedProperty ShootAtTarget = myScript.FindProperty("ShootAtTarget");
+        SerializedProperty ChooseShootDirections = myScript.FindProperty("ChooseShootDirections");
+        SerializedProperty ShootLeft = myScript.FindProperty("ShootLeft");
+        SerializedProperty ShootUp = myScript.FindProperty("ShootUp");
+        SerializedProperty ShootRight = myScript.FindProperty("ShootRight");
+
+        EditorGUILayout.Space(); EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Cross Base Setup", EditorStyles.boldLabel);
+
+        EditorGUILayout.PropertyField(PlayerController);
+
+        if (PlayerController.objectReferenceValue == null) {
+            EditorGUILayout.HelpBox(_playerControllerError, MessageType.Error);
+        }
+
+        EditorGUILayout.PropertyField(BulletToSpawn);
+
+        if (BulletToSpawn.objectReferenceValue == null) {
+            EditorGUILayout.HelpBox(_bulletToSpawnError, MessageType.Error);
+        }
+
+        EditorGUILayout.PropertyField(AttackDelay);
+
+        if (AttackDelay.floatValue <= 0) {
+            EditorGUILayout.HelpBox(_attackDelayError, MessageType.Error);
+        }
+
+        EditorGUILayout.PropertyField(ShootAlways);
+
+        if (ShootAlways.boolValue) {
+            EditorGUILayout.HelpBox(_shootAlwaysInfo, MessageType.Info);
+        } else {
+            EditorGUILayout.PropertyField(DetectionBlock);
+
+            if (DetectionBlock.boundsValue.extents.x <= 0 || DetectionBlock.boundsValue.extents.y <= 0) {
+                EditorGUILayout.HelpBox(_rangeDetectionSizeError, MessageType.Error);
+            }
+        }
+
+        EditorGUILayout.PropertyField(ShootRangeBlock);
+
+        if (ShootRangeBlock.boundsValue.extents.x <= 0 || ShootRangeBlock.boundsValue.extents.y <= 0) {
+            EditorGUILayout.HelpBox(_rangeDetectionSizeError, MessageType.Error);
+        }
+        
+
+        EditorGUILayout.PropertyField(ShootAtTarget);
+
+        if (!ShootAtTarget.boolValue) {
+            EditorGUILayout.PropertyField(ChooseShootDirections);
+
+            if (ChooseShootDirections.boolValue) {
+                EditorGUILayout.PropertyField(ShootLeft);
+                EditorGUILayout.PropertyField(ShootUp);
+                EditorGUILayout.PropertyField(ShootRight);
+            } else {
+                EditorGUILayout.HelpBox(_choseShootDirectionsInfo, MessageType.Info);
+            }
+        } else {
+            EditorGUILayout.HelpBox(_shootAtTargetInfo, MessageType.Info);
+        }
+
+        myScript.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Editor/Player/PlayerCustomEditor.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using CustomPlugins;
+
+[CustomEditor(typeof(Player.Player))]
+public abstract class PlayerCustomEditor : Editor {
+
+    private const string _playerInfo = "Never use the Player Script alone, you must inheritage from another class.";
+    private const string _animatorInfo = "If any of this State Name is empty it will not be access by the script.";
+    private const string _groundAndCeilingWarning = "This GameObject must have two childs called GroundCheck and CeilingChek with only a transform component for this to work propertly.";
+    private const string _groundAndCeilingInfo = "If the debugger setup is on, you will se those detection areas as red circles.";
+    private const string _groundAndCeilingError = "The Ground and Ceiling values must be bigger than 0 if you are using this kind of detection.";
+    private const string _audioInfo = "If any audio is empty it will simply won't make any sound for that clip.";
+    private const string _audioWarning = "This GameObject must have an AudioSource for the AudioClips to work.";
+    private const string _debugInfo = "It will show information on the Scene but not in the actual Game.";
+
+    public override void OnInspectorGUI() {
+
+        SerializedObject myScript = new SerializedObject(target);
+        SerializedProperty Life = myScript.FindProperty("Life");
+        SerializedProperty AttackDamage = myScript.FindProperty("AttackDamage");
+        SerializedProperty WalkingSpeed = myScript.FindProperty("WalkingSpeed");
+        SerializedProperty JumpForce = myScript.FindProperty("JumpForce");
+        SerializedProperty AirControl = myScript.FindProperty("AirControl");
+        SerializedProperty UseGroundAndCeilingDetection = myScript.FindProperty("UseGroundAndCeilingDetection");
+        SerializedProperty WhatIsGround = myScript.FindProperty("WhatIsGround");
+        SerializedProperty GroundedRadius = myScript.FindProperty("GroundedRadius");
+        SerializedProperty CeilingRadius = myScript.FindProperty("CeilingRadius");
+        SerializedProperty StateVariableName = myScript.FindProperty("StateVariableName");
+        SerializedProperty SpeedVariableName = myScript.FindProperty("SpeedVariableName");
+        SerializedProperty vSpeedVariableName = myScript.FindProperty("vSpeedVariableName");
+        SerializedProperty GroundVariableName = myScript.FindProperty("GroundVariableName");
+        SerializedProperty SoundIdle = myScript.FindProperty("SoundIdle");
+        SerializedProperty SoundWalking = myScript.FindProperty("SoundWalking");
+        SerializedProperty SoundAttacking = myScript.FindProperty("SoundAttacking");
+        SerializedProperty SoundJumping = myScript.FindProperty("SoundJumping");
+        SerializedProperty SoundDead = myScript.FindProperty("SoundDead");
+        SerializedProperty SoundVictory = myScript.FindProperty("SoundVictory");
+        SerializedProperty SoundHurt = myScript.FindProperty("SoundHurt");
+        SerializedProperty SoundIdleAir = myScript.FindProperty("SoundIdleAir");
+        SerializedProperty EditorDebugMode = myScript.FindProperty("EditorDebugMode");
+
+        EditorGUILayout.HelpBox(_playerInfo, MessageType.None);
+
+        EditorGUILayout.Space(); EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Player Base Setup", EditorStyles.boldLabel);
+
+        EditorGUILayout.PropertyField(Life);
+        EditorGUILayout.PropertyField(AttackDamage);
+
+        EditorGUILayout.Space(); EditorGUILayout.Space();
+
+        EditorGUILayout.PropertyField(WalkingSpeed);
+        EditorGUILayout.PropertyField(JumpForce);
+        EditorGUILayout.PropertyField(AirControl);
+        EditorGUILayout.PropertyField(UseGroundAndCeilingDetection);
+
+        if (UseGroundAndCeilingDetection.boolValue) {
+
+            EditorGUILayout.Space(); EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Player Ground and Ceiling Setup", EditorStyles.boldLabel);
+
+            EditorGUILayout.HelpBox(_groundAndCeilingWarning, MessageType.Warning);
+            EditorGUILayout.HelpBox(_groundAndCeilingInfo, MessageType.Info);
+            
+            EditorGUILayout.PropertyField(WhatIsGround);
+            EditorGUILayout.PropertyField(GroundedRadius);
+            EditorGUILayout.PropertyField(CeilingRadius);
+
+            if (GroundedRadius.floatValue <= 0 || CeilingRadius.floatValue <= 0) {
+                EditorGUILayout.HelpBox(_groundAndCeilingError, MessageType.Info);
+            }
+        }
+
+        EditorGUILayout.Space(); EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Player Animator Setup", EditorStyles.boldLabel);
+
+        EditorGUILayout.HelpBox(_animatorInfo, MessageType.Info);
+
+        EditorGUILayout.PropertyField(StateVariableName);
+        EditorGUILayout.PropertyField(SpeedVariableName);
+        EditorGUILayout.PropertyField(vSpeedVariableName);
+        EditorGUILayout.PropertyField(GroundVariableName);
+
+        EditorGUILayout.Space(); EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Player Audio Setup", EditorStyles.boldLabel);
+
+        EditorGUILayout.HelpBox(_audioWarning, MessageType.Warning);
+        EditorGUILayout.HelpBox(_audioInfo, MessageType.Info);
+
+
+        EditorGUILayout.PropertyField(SoundIdle);
+        EditorGUILayout.PropertyField(SoundWalking);
+        EditorGUILayout.PropertyField(SoundAttacking);
+        EditorGUILayout.PropertyField(SoundJumping);
+        EditorGUILayout.PropertyField(SoundDead);
+        EditorGUILayout.PropertyField(SoundVictory);
+        EditorGUILayout.PropertyField(SoundHurt);
+        EditorGUILayout.PropertyField(SoundIdleAir);
+
+        EditorGUILayout.Space(); EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Player Debug Setup", EditorStyles.boldLabel);
+
+        EditorGUILayout.HelpBox(_debugInfo, MessageType.Info);
+
+        EditorGUILayout.PropertyField(EditorDebugMode);
+
+        myScript.ApplyModifiedProperties();
+
+        ExtraGUI();
+    }
+
+    public abstract void ExtraGUI();
+
+}
